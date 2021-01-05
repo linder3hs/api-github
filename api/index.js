4298ed4d2f3cdf4ce7fb8de35e9f86532c53a9d3
linder3hs <linderhassinger00@gmail.com>
2021-01-05 11:12:13 -0500

    router documentation

--- api/index.js
+++ api/index.js
@@ -9,10 +9,15 @@ const cors = require("cors");
 
 const swaggerDoc = require("./swagger.json");
 
+const options = {
+  customCssUrl:
+    "https://firebasestorage.googleapis.com/v0/b/recognizedios.appspot.com/o/style.css?alt=media&token=d5b95bc7-17d7-4437-969e-a6ad36b13839",
+};
+
 app.use(cors());
 
 app.use("/api/github", github);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc, options));
 
 app.listen(config.api.port, () => {
   console.log(`Listen in port ${config.api.port}`);
